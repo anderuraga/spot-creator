02dba2c7ca05cd6b14ba55ee649bc1c0f44392bc
anderuraga <ander.uraga@gmail.com>
2017-11-07 09:25:59 +0100

    viento y organizar carpetas

---
+++ js/compass.js
@@ -0,0 +1,50 @@
+var canvas = document.getElementById('myCanvas');
+var canvasWind = document.getElementById('myCanvasWind');
+paint_compas( Math.PI, Math.PI*2, canvas);
+paint_compas( Math.PI, Math.PI*2, canvasWind);
+
+function paint_compas(start, end, canva){
+      console.debug("angulos %s %s",start,end);
+      var ctx = canva.getContext('2d');
+      ctx.clearRect(0, 0, canva.width, canva.height);
+
+      var x = 100;
+      var y = 100;
+      var radius = 75;
+      var antiClockwise = false;
+
+      ctx.beginPath();
+      ctx.arc(x, y, radius, start*Math.PI, end*Math.PI, antiClockwise);
+      ctx.lineTo(x, y);
+      ctx.lineWidth = 5;
+      ctx.fillStyle = "rgba(50, 120, 150, 0.8)";
+      ctx.strokeStyle = "rgba(50, 120, 150, 0.8)";
+      ctx.closePath();
+      ctx.fill();
+}
+
+var sStart = document.getElementById('start');
+var sEnd = document.getElementById('end');
+var sStartWind = document.getElementById('startWind');
+var sEndWind = document.getElementById('endWind');
+
+var angleStart = 0;
+var angleEnd = 0;
+
+sStart.onchange = function(e){
+  angleStart = e.target.value;
+  paint_compas(angleStart,angleEnd, canvas);
+};
+sEnd.onchange = function(e){
+  angleEnd = e.target.value;
+  paint_compas(angleStart,angleEnd, canvas);
+};
+
+sStartWind.onchange = function(e){
+  angleStart = e.target.value;
+  paint_compas(angleStart,angleEnd, canvasWind);
+};
+sEndWind.onchange = function(e){
+  angleEnd = e.target.value;
+  paint_compas(angleStart,angleEnd, canvasWind);
+};
